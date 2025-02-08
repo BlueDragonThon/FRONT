fe0b5f6861b52ecd3a5c2de30ced462393b8562a
CLOUDYUL <gykim1524@gmail.com>
2025-02-09 00:04:32 +0900

    fix: import 주소 수정

--- lib/screens/search_univ.dart
+++ lib/screens/search_univ.dart
@@ -1,5 +1,5 @@
 // search_univ.dart
-import 'package:bluedragonthon/services/search_service.dart';
+import 'package:bluedragonthon/services/search_api_service.dart';
 import 'package:bluedragonthon/utils/university_model.dart';
 import 'package:bluedragonthon/widgets/university_widgets.dart';
 import 'package:flutter/material.dart';
